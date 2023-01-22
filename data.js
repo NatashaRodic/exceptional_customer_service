d371de2c08918d032a19542c45290501a421c241
Natasha Rodic <natashawolt@gmail.com>
2023-01-21 20:21:50 -0500

    HTML structure

---
+++ data.js
@@ -0,0 +1,47 @@
+const CATEGORIES = [
+  { name: "Mozzarella", color: "#3b82f6" },
+  { name: "Parmesan", color: "#16a34a" },
+  { name: "Cheddar", color: "#ef4444" },
+  { name: "Brie", color: "#eab308" },
+  { name: "Swiss", color: "#db2777" },
+  { name: "Gruyere", color: "#14b8a6" },
+  { name: "Feta", color: "#f97316" },
+  { name: "Gouda", color: "#8b5cf6" },
+];
+
+const initialFacts = [
+  {
+    id: 1,
+    text: "Chalk and cheese gouda stinking bishop babybel when the cheese comes out everybody's happy cheese triangles port-salut swiss.",
+    source: "http://www.cheeseipsum.co.uk/",
+    category: "Mozzarella",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Queso stinking bishop brie swiss cheese and biscuits cauliflower cheese cheese triangles swiss. Monterey jack croque monsieur squirty cheese emmental caerphilly cheddar lancashire cottage cheese.",
+    source: "http://www.cheeseipsum.co.uk/",
+    category: "Parmesan",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Fromage frais feta chalk and cheese. Swiss fromage frais cheese and biscuits croque monsieur bavarian bergkase cheese strings red leicester goat. ",
+    source: "http://www.cheeseipsum.co.uk/",
+    category: "Cheddar",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
+
+// LINK TO APP SAMPLE DATA: https://docs.google.com/spreadsheets/d/1eeldcA_OwP4DHYEvjG0kDe0cRys-cDPhc_E9P9G1e3I/edit#gid=0
+
+// 👍 🤯 ⛔️
