3f4310ef8640c131938be80d8cd3e6a81263ff99
Natasha Rodic <natashawolt@gmail.com>
2023-02-02 22:03:21 -0500

    Creates react components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      {/* HEADER  */}
+      <header className="header">
+        <div className="logo">
+          <img
+            src="box_bozzuto.png"
+            height="68"
+            width="100"
+            alt="Today I helped Logo"
+          />
+        </div>
+        <div className="headerimages">
+          <img
+            src="person.png"
+            height="100"
+            width="200"
+            alt="Today I helped Logo"
+          />
+          <button className="btn btn-large btn-open">Share a story!</button>
+        </div>
       </header>
-    </div>
+      <NewStoryForm />
+      <main class="main">
+        <CategoryFilter />
+        <StoryList />
+      </main>
+    </>
   );
 }
-
+function NewStoryForm() {
+  return <form className="story-form">Story Form</form>;
+}
+function CategoryFilter() {
+  return <aside>Category filter</aside>;
+}
+function StoryList() {
+  return <section>Story List</section>;
+}
 export default App;
