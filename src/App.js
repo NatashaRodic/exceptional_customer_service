5952e5397f5f806089b72b4d7843b99967afcd1b
Natasha Rodic <natashawolt@gmail.com>
2023-02-05 20:10:32 -0500

    Divides Interface

--- src/App.js
+++ src/App.js
@@ -1,4 +1,48 @@
+import { createFactory } from "react";
 import "./style.css";
+const CATEGORIES = [
+  { name: "Mozzarella", color: "#3b82f6" },
+  { name: "Parmesan", color: "#16a34a" },
+  { name: "Cheddar", color: "#ef4444" },
+  { name: "Brie", color: "#eab308" },
+  { name: "Swiss", color: "#db2777" },
+  { name: "Gruyere", color: "#14b8a6" },
+  { name: "Feta", color: "#f97316" },
+  { name: "Gouda", color: "#8b5cf6" },
+];
+
+const initialStories = [
+  {
+    id: 1,
+    text: "Chalk and cheese gouda stinking bishop babybel when the cheese comes out everybody's happy cheese triangles port-salut swiss.",
+    source: "http://www.cheeseipsum.co.uk/",
+    category: "Pets",
+    votesLike: 25,
+    votesDislike: 1,
+    votesMindblowing: 2,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Queso stinking bishop brie swiss cheese and biscuits cauliflower cheese cheese triangles swiss. Monterey jack croque monsieur squirty cheese emmental caerphilly cheddar lancashire cottage cheese.",
+    source: "http://www.cheeseipsum.co.uk/",
+    category: "Requests",
+    votesLike: 8,
+    votesDislike: 0,
+    votesMindblowing: 1,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Fromage frais feta chalk and cheese. Swiss fromage frais cheese and biscuits croque monsieur bavarian bergkase cheese strings red leicester goat. ",
+    source: "http://www.cheeseipsum.co.uk/",
+    category: "Packages",
+    votesLike: 8,
+    votesDislike: 3,
+    votesMindblowing: 1,
+    createdIn: 2015,
+  },
+];
 
 function App() {
   return (
@@ -24,7 +68,7 @@ function App() {
         </div>
       </header>
       <NewStoryForm />
-      <main class="main">
+      <main className="main">
         <CategoryFilter />
         <StoryList />
       </main>
@@ -38,6 +82,30 @@ function CategoryFilter() {
   return <aside>Category filter</aside>;
 }
 function StoryList() {
-  return <section>Story List</section>;
+  const stories = initialStories;
+  return (
+    <section>
+      <ul className="story-list">
+        {stories.map((story) => (
+          <li className="story">
+            <p>
+              {story.text}
+              <a className="source" href={story.source} target="_blank">
+                (Source)
+              </a>
+            </p>
+            <span className="category" style={{ backgroundColor: "#3b82f6" }}>
+              {story.category}
+            </span>
+            <div className="buttons-like-dislike">
+              <button>👍 {story.votesLike}</button>
+              <button>👎 {story.votesDislike}</button>
+              <button>🤯 {story.votesMindblowing}</button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
 export default App;
