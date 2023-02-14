3539227aaf14fe0b7045de63cc18a97562ca8a85
Natasha Rodic <natashawolt@gmail.com>
2023-02-13 20:34:30 -0500

    Creates the Header of the page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,5 @@
-import { createFactory } from "react";
+import { createFactory, useState } from "react";
 import "./style.css";
-const CATEGORIES = [
-  { name: "Mozzarella", color: "#3b82f6" },
-  { name: "Parmesan", color: "#16a34a" },
-  { name: "Cheddar", color: "#ef4444" },
-  { name: "Brie", color: "#eab308" },
-  { name: "Swiss", color: "#db2777" },
-  { name: "Gruyere", color: "#14b8a6" },
-  { name: "Feta", color: "#f97316" },
-  { name: "Gouda", color: "#8b5cf6" },
-];
 
 const initialStories = [
   {
@@ -45,29 +35,14 @@ const initialStories = [
 ];
 
 function App() {
+  const [showForm, setShowForm] = useState();
+
   return (
     <>
-      {/* HEADER  */}
-      <header className="header">
-        <div className="logo">
-          <img
-            src="box_bozzuto.png"
-            height="68"
-            width="100"
-            alt="Today I helped Logo"
-          />
-        </div>
-        <div className="headerimages">
-          <img
-            src="person.png"
-            height="100"
-            width="200"
-            alt="Today I helped Logo"
-          />
-          <button className="btn btn-large btn-open">Share a story!</button>
-        </div>
-      </header>
-      <NewStoryForm />
+      <Header showForm={showForm} setShowForm={setShowForm} />
+
+      {showForm ? <NewStoryForm /> : null}
+
       <main className="main">
         <CategoryFilter />
         <StoryList />
@@ -75,11 +50,66 @@ function App() {
     </>
   );
 }
+
+function Header({ showForm, setShowForm }) {
+  const appTitle = "Bozzuto Experience";
+  return (
+    <header className="header">
+      <div className="logo">
+        <img
+          src="box_bozzuto.png"
+          height="68"
+          width="100"
+          alt="Today I helped Logo"
+        />
+      </div>
+      <div className="headerimages">
+        <img
+          src="person.png"
+          height="100"
+          width="200"
+          alt="Today I helped Logo"
+        />
+        <button
+          className="btn btn-large btn-open"
+          onClick={() => setShowForm((show) => !show)}
+        >
+          {showForm ? "Close" : "Share a fact"}
+        </button>
+      </div>
+    </header>
+  );
+}
 function NewStoryForm() {
   return <form className="story-form">Story Form</form>;
 }
+
+const CATEGORIES = [
+  { name: "Pets", color: "#3b82f6" },
+  { name: "Packages", color: "#16a34a" },
+  { name: "Requests", color: "#ef4444" },
+  { name: "Brie", color: "#eab308" },
+  { name: "Swiss", color: "#db2777" },
+  { name: "Gruyere", color: "#14b8a6" },
+  { name: "Feta", color: "#f97316" },
+  { name: "Gouda", color: "#8b5cf6" },
+];
+
 function CategoryFilter() {
-  return <aside>Category filter</aside>;
+  return (
+    <aside>
+      <ul>
+        <li className="category2">
+          <button className="btn btn-all-categories">All</button>
+        </li>
+        {CATEGORIES.map((category) => (
+          <li key={category.name} className="category2">
+            <button className="btn btn-all-categories">{category.name}</button>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
 }
 function StoryList() {
   const stories = initialStories;
@@ -87,25 +117,37 @@ function StoryList() {
     <section>
       <ul className="story-list">
         {stories.map((story) => (
-          <li className="story">
-            <p>
-              {story.text}
-              <a className="source" href={story.source} target="_blank">
-                (Source)
-              </a>
-            </p>
-            <span className="category" style={{ backgroundColor: "#3b82f6" }}>
-              {story.category}
-            </span>
-            <div className="buttons-like-dislike">
-              <button>👍 {story.votesLike}</button>
-              <button>👎 {story.votesDislike}</button>
-              <button>🤯 {story.votesMindblowing}</button>
-            </div>
-          </li>
+          <Story key={story.id} story={story} />
         ))}
       </ul>
     </section>
   );
 }
+function Story({ story }) {
+  return (
+    <li className="story">
+      <p>
+        {story.text}
+        <a className="source" href={story.source} target="_blank">
+          (Source)
+        </a>
+      </p>
+      <span
+        className="category"
+        style={{
+          backgroundColor: CATEGORIES.find((cat) => cat.name === story.category)
+            .color,
+        }}
+      >
+        {story.category}
+      </span>
+      <div className="buttons-like-dislike">
+        <button>👍 {story.votesLike}</button>
+        <button>👎 {story.votesDislike}</button>
+        <button>🤯 {story.votesMindblowing}</button>
+      </div>
+    </li>
+  );
+}
+
 export default App;
